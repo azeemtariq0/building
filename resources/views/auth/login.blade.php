02144c5c044dd41c9455eb332a246113f5d4bad7
Azeem <az.tariq03@gmail.com>
2023-10-13 23:34:09 +0500

    Initial comments

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.auth')
+
+@section('content')
+
+<div class="padding-15">
+    <div class="login-box">
+        <!-- login form -->
+        <form method="POST" action="{{ route('login') }}" class="sky-form boxed">
+            @csrf
+            <header><i class="fa fa-users"></i> {{ __('Sign In') }}</header>
+                    <!--
+                    <div class="alert alert-danger noborder text-center weight-400 nomargin noradius">
+                        Invalid Email or Password!
+                    </div>
+
+                    <div class="alert alert-warning noborder text-center weight-400 nomargin noradius">
+                        Account Inactive!
+                    </div>
+
+                    <div class="alert alert-default noborder text-center weight-400 nomargin noradius">
+                        <strong>Too many failures!</strong> <br />
+                        Please wait: <span class="inlineCountdown" data-seconds="180"></span>
+                    </div>
+                -->
+
+                @error('email')
+                <div class="alert alert-danger noborder text-center weight-400 nomargin noradius">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                @error('password')
+                <div class="alert alert-danger noborder text-center weight-400 nomargin noradius">
+                    {{ $message }}
+                </div>
+                @enderror
+                <fieldset>  
+                    <section>
+                        <label class="label">{{ __('Email Address') }}</label>
+                        <label class="input">
+                            <i class="icon-append fa fa-envelope"></i>
+                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <span class="tooltip tooltip-top-right">Email Address</span>
+                        </label>
+                    </section>
+                    <section>
+                        <label class="label">{{ __('Password') }}</label>
+                        <label class="input">
+                            <i class="icon-append fa fa-lock"></i>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <b class="tooltip tooltip-top-right">Type your Password</b>
+                        </label>
+
+                        <label class="checkbox">
+                            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}><i></i>{{ __('Keep me logged in') }}
+                        </label>
+                    </section>
+                </fieldset>
+                <footer>
+                    <button type="submit" class="btn btn-primary pull-right">{{ __('Sign In') }}</button>
+                    <div class="forgot-password pull-left">
+                       @if (Route::has('password.request'))
+                       <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}?</a> <br />
+                       @endif
+                       <!-- <a href="page-register.html"><b>Need to Register?</b></a> -->
+                   </div>
+               </footer>
+           </form>
+           <!-- /login form -->
+           <hr />
+       </div>
+   </div>
+   
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,123 @@
+<?php 
+$page_title = "";
+$slug = "";
+if(!empty($data)){
+    if(!empty($data['page_management'])){
+        if($data['page_management']['page_title'] != ""){
+            $page_title = $data['page_management']['page_title']; 
+        }
+
+        if($data['page_management']['slug'] != ""){
+            $slug = $data['page_management']['slug']; 
+        }
+
+    }
+}
+?>
+<!doctype html>
+    <html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <title>{{ config('app.name', 'BMA Portal') }} - {{$page_title}}</title>
+        <meta name="description" content="" />
+        <meta name="Author" content="Ghulam Rasool [imgrasool@gmail.com]" />
+
+        <!-- mobile settings -->
+        <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0" />
+
+        <!-- WEB FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&amp;subset=latin,latin-ext,cyrillic,cyrillic-ext" rel="stylesheet" type="text/css" />
+
+        <!-- CORE CSS -->
+        <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- THEME CSS -->
+        <link href="{{ asset('assets/css/essentials.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/layout.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/color_scheme/green.css') }}" rel="stylesheet" type="text/css" id="color_scheme" />
+
+
+        <!-- Call Jquery -->
+        <script type="text/javascript" src="{{ asset('assets/plugins/jquery/jquery-2.1.4.min.js')}}"></script>
+
+
+        @yield('pagelevelstyle')
+    </head>
+    <!--
+        .boxed = boxed version
+    -->
+    <body>
+
+
+        <!-- WRAPPER -->
+        <div id="wrapper" class="clearfix">
+
+            <!-- ASIDE 
+            Keep it outside of #wrapper (responsive purpose)
+        -->
+        @include('layouts.sidebar')
+        <!-- /ASIDE -->
+
+        <!-- HEADER -->
+        @include('layouts.header')
+        <!-- /HEADER -->
+
+        <!-- MIDDLE -->
+<!--         <section id="middle">
+
+
+            <header id="page-header">
+                <h1>Upload New Research</h1>
+                <ol class="breadcrumb">
+                    <li><a href="{{ route('research.index') }}">Research</a></li>
+                    <li class="active">Upload</li>
+                </ol>
+            </header> -->
+
+
+            <section id="middle">
+
+              <!-- page title -->
+              <header id="page-header">
+                <h1>
+
+                    <?php 
+                    echo $page_title;
+                ?></h1>
+                <ol class="breadcrumb">
+                    <li><a href="{{url('/').'/'.\Request::segment(1)}}">{{ucfirst(\Request::segment(1));}}</a></li>
+                    @if(!empty($slug))
+                    <li class="active">{{ucfirst($slug);}}</li>
+                    @endif
+
+
+                    
+                </ol>
+            </header>
+            <!-- /page title -->
+
+            @yield('content')
+            <!-- /MIDDLE -->
+        </section>
+
+    </div>
+
+
+
+
+    <!-- JAVASCRIPT FILES -->
+    <!--     <script type="text/javascript">var plugin_path = 'assets/plugins/';</script> -->
+    <script type="text/javascript">var plugin_path = "{{ URL::asset('assets/plugins/') }}/";</script>
+    <!-- <script type="text/javascript" src="{{ asset('assets/plugins/jquery/jquery-2.1.4.min.js')}}"></script> -->
+    <script type="text/javascript" src="{{ asset('assets/js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/custom.js') }}"></script>
+    <!-- STYLESWITCHER - REMOVE -->
+    <!-- <script async type="text/javascript" src="{{ asset('assets/plugins/styleswitcher/styleswitcher.js') }}"></script> -->
+    <script>
+        
+        // $('.alert').delay(5000).fadeOut('slow');
+    </script>
+    @yield('pagelevelscript')
+</body>
+</html>
